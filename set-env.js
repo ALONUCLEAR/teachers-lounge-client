203ff7a26046cc79e72dbc87088429878b5516a1
ALONUCLEAR <alonrazka58@gmail.com>
2025-02-28 20:23:52 +0200

    fix: make environment variables fitting of secrets

---
+++ set-env.js
@@ -0,0 +1,33 @@
+const optional = (val) => val ? `'${val}'` : `undefined` 
+
+const trimLines = (input) => input.split('\n').map(line => line.trim()).join('\n');
+
+const setEnv = () => {
+    const writeFile = require('fs').writeFile;
+    const targetPath = './src/environments/environment.ts';
+    require('dotenv').config();
+
+    const envConfigFile = trimLines(`import { Config } from './types';
+    import { defaultEnv } from './default';
+
+    export const environment: Config = {
+      ...defaultEnv,
+      env: ${optional(process.env['ENV'])},
+      govUrl: ${optional(process.env['GOV_URL'])},
+      serverUrl: ${optional(process.env['SERVER_URL'])},
+      encryptionKey: ${optional(process.env['ENCRYPTION_KEY'])}
+    };    
+  `);
+
+    writeFile(targetPath, envConfigFile, (err) => {
+      if (err) {
+        console.error("Error writing environment file", err);
+        throw err;
+      } else {
+        console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+      }
+    });
+  };
+  
+setEnv();
+  
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 import { Config } from './types';
+import { defaultEnv } from './default';
 
 export const environment: Config = {
+  ...defaultEnv,
   env: 'Prod',
   govUrl: 'https://data.gov.il/api/3/action/datastore_search',
   serverUrl: 'https://prod-server',
-  encryptionKey: 'store-it-somewhere-else'
+  encryptionKey: ''
 };
